6990486906f9b2747edfb16ecdf57bb9dda5fd1b
yaniraton <yanirahahah2007@gmail.com>
2024-07-04 00:27:05 +0300

    Implemented Controller class - defined its fields and method

--- server/Controller.py
+++ server/Controller.py
@@ -1,19 +1,63 @@
-from pydualsense import pydualsense, TriggerModes
+from pydualsense import pydualsense
+import Constants
 
-def cross_pressed(state):
-    print(state)
+class Controller:
+    def __init__(self):
+        self.ds = pydualsense()
+        self.leftY = 0.0
+        self.leftX = 0.0
+        self.rightX = 0.0
+    
+    
+    def set_left_values(self, leftX : float, leftY: float):
+        """
+        This function sets the values of the left joystick, between -1 and 1.
 
-def joystick(stateX, stateY):
-    print(f'joystick {stateX} {stateY}')
+        Args:
+            leftX (float): The x axis value of the left joystick, between -128 and 128.
+            leftY (float): The y axis value of the left joystick, between -128 and 128.
+        """
+        self.leftY = leftY / Constants.HALF_BYTE
+        self.leftX = leftX / Constants.HALF_BYTE
+    
+    
+    def set_right_values(self, rightX: float, rightY: float):
+        """
+        This function sets the values of the right joystick, between -1 and 1.
 
+        Args:
+            rightX (float): The x axis value of the right joystick, between -128 and 128.
+            rightY (float): The y axis value of the right joystick, between -128 and 128.
+        """        
+        self.rightX = rightX / Constants.HALF_BYTE
+    
+    
+    def connect(self) -> bool:
+        """
+        This function connects the joystick to the program, and sets the event handlers to listen to the joysticks changes.
 
-ds = pydualsense() # open controller
-ds.init() # initialize controller
+        Returns:
+            bool: Returns whether or not the connection was succsessful.
+        """        
+        try:
+            self.ds.init()
+        except Exception as e:  # Catch any Exception
+            if str(e) == "No device detected":  # Check for the exact message
+                print("Error: No contoler was found.")
+            else:
+                print("An unexpected error occurred:", e)
+            return False
+        self.ds.left_joystick_changed += self.set_left_values
+        self.ds.right_joystick_changed += self.set_right_values
+        return True
 
-ds.cross_pressed += cross_pressed
-ds.light.setColorI(255,0,0) # set touchpad color to red
-ds.triggerL.setMode(TriggerModes.Rigid)
-ds.triggerL.setForce(1, 255)
-ds.close() # closing the controller
-ds.left_joystick_changed += joystick
+    
+    def get_joisticks_values(self) -> tuple:
+        """
+        This function gets the current state of the joysticks.
 
+        Returns:
+            tuple: A tuple which contains the left joystick Y, left joystick X and right joystick X in that exact order.
+            All of them are values between -1 and 1.
+        """         
+        return (self.leftY, self.leftX, self.rightX)
